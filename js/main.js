13bd935657e200fe616608480066b19a92ad18a4
darkpaha63 <averin.samara@gmail.com>
2021-07-14 15:29:25 +0400

    create: added yandex map api in hotel section

--- js/main.js
+++ js/main.js
@@ -14,3 +14,15 @@ const swiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+
+// Подключение Яндекс API
+ymaps.ready(init);
+    function init(){
+        var myMap = new ymaps.Map("map", {
+            center: [7.890741, 98.294690],
+            zoom: 17
+        });
+        var point = new YMaps.GeoPoint(7.890741, 98.294690);
+        var placemark = new YMaps.Placemark(point);
+        map.addOverlay(placemark);
+    }
