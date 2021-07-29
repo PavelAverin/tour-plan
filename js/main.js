7be9335214f4dc91b2c22509a5c87fe275b3f07e
darkpaha63 <averin.samara@gmail.com>
2021-07-29 16:10:19 +0400

    fix: fix 1 & 2 errors

--- js/main.js
+++ js/main.js
@@ -1,161 +1,75 @@
-$(document).ready(function () {
-  const hotelSLider = new Swiper('.hotel-slider', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
-    },
-  
-    // Keyboard navigation
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-  });
-  
-  const reviewsSlider = new Swiper('.reviews-slider', {
-    // Optional parameters
-    loop: true,
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.reviews-slider__button--next',
-      prevEl: '.reviews-slider__button--prev',
-    },
-  
-    // Keyboard navigation
-    keyboard: {
-      enabled: true,
-      onlyInViewport: false,
-    },
-  });
-  
-  $('.parallax-window').parallax({
-    imageSrc: './img/newsletter-bg.webp',
-    speed: 0.6,
-  });
+$(document).ready(function(){const hotelSLider=new Swiper('.hotel-slider',{loop:true,navigation:{nextEl:'.hotel-slider__button--next',prevEl:'.hotel-slider__button--prev',},keyboard:{enabled:true,onlyInViewport:false,},});const reviewsSlider=new Swiper('.reviews-slider',{loop:true,navigation:{nextEl:'.reviews-slider__button--next',prevEl:'.reviews-slider__button--prev',},keyboard:{enabled:true,onlyInViewport:false,},});
+$('.parallax-window').parallax({imageSrc:'./img/newsletter-bg.webp',speed:0.6,});
+ymaps.ready(init);function init(){var myMap=new ymaps.Map("map",{center:[7.890741,98.294690],zoom:17});var myPlacemark=new ymaps.Placemark([7.890741,98.294690]);myMap.geoObjects.add(myPlacemark)};
 
+var menuButton=document.querySelector(".menu-button");
+menuButton.addEventListener('click',function(){
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  document.querySelector(".navbar-top__menu-button").classList.toggle("menu__button--hidden");
+  document.querySelector(".close").classList.toggle("close--visible")
+});
 
-  // Подключение Яндекс API   
-  ymaps.ready(init);
-  function init(){
-    var myMap = new ymaps.Map("map", {
-        center: [7.890741, 98.294690],
-        zoom: 17
-    });
-  
-    var myPlacemark = new ymaps.Placemark([7.890741, 98.294690]);
-  
-    myMap.geoObjects.add(myPlacemark);
-  };
-  
-
-  // скрипт разворачивания мобильного меню и смены кнопки на крестик
-  var menuButton = document.querySelector(".menu-button");
-  menuButton.addEventListener('click', function () {
-    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
-    document.querySelector(".navbar-top__menu-button").classList.toggle("menu__button--hidden");
-    document.querySelector(".close").classList.toggle("close--visible");
-  });
-  
-
-  // скрипт сворачивания мобильного меню и смены кнопки на бургер
-  var menuClose = document.querySelector(".close");
-  menuClose.addEventListener('click', function () {
+  var menuClose=document.querySelector(".close");
+  menuClose.addEventListener('click',function(){
     document.querySelector(".close").classList.toggle("close--visible");
     document.querySelector(".navbar-top__menu-button").classList.toggle("menu__button--hidden");
-    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
   });
 
-
-  // скрипт для модального окна
-  var modalButton = $('[data-toggle=modal]');
-  var closeModalButton = $(".modal__close");
-
-
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
+  var modalButton=$('[data-toggle=modal]');
+  var closeModalButton=$(".modal__close");
+  modalButton.on('click',openModal);
+  closeModalButton.on('click',closeModal);
   
-
-  // функция для открытия модального окна
-  function openModal() {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-
+  function openModal(){
+    var modalOverlay=$(".modal__overlay");
+    var modalDialog=$(".modal__dialog");
     modalOverlay.addClass('modal__overlay--visible');
     modalDialog.addClass('modal__dialog--visible');
+    offScroll();
   };
 
-  // функция для закрытия модального окна
-  function closeModal(event) {
+  function closeModal(event){
     event.preventDefault();
-
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-
+    var modalOverlay=$(".modal__overlay");
+    var modalDialog=$(".modal__dialog");
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
+    $(window).unbind('scroll');
   };
 
-  // функция для закрытия модального окна при нажатии Escape
-  $(document).keyup(function(e) {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    if (e.key === "Escape") {      
-      modalOverlay.removeClass('modal__overlay--visible');
-      modalDialog.removeClass('modal__dialog--visible');
-    };
+$(document).keyup(function(e){
+  var modalOverlay=$(".modal__overlay");
+  var modalDialog=$(".modal__dialog");
+  if(e.key==="Escape"){
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+    $(window).unbind('scroll');
+}
+});
+
+$(document).mouseup(function(e){
+  var modalOverlay=$(".modal__overlay");
+  var modalDialog=$(".modal__dialog");
+  if(!modalDialog.is(e.target)&&modalDialog.has(e.target).length===0){
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+    $(window).unbind('scroll');
+  }
+});
+
+function offScroll () {
+  var winScrollTop = $(window).scrollTop();
+  $(window).bind('scroll', function () {
+    $(window).scrollTop(winScrollTop);
   });
+};
 
-  // функция для закрытия модального окна при нажатии на overlay
-  $(document).mouseup(function (e) {
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0) {
-      modalOverlay.removeClass('modal__overlay--visible');
-      modalDialog.removeClass('modal__dialog--visible');
-    };
-  });
+$('.form').each(function(){$(this).validate({errorClass:"invalid",rules:{email:{pattern:/[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}/i,},},messages:{name:{required:"Please specify your name",minlength:2,},email:{required:"We need your email address to contact you",email:"Your email address must be in the format of name@domain.com",pattern:'Format for email: name@domain.com',},phone:{required:"Please specify your phone",}},})});
 
-
-  // Обработка форм
-  $('.form').each(function () {
-    $(this).validate({
-      errorClass: "invalid",
-      rules: {
-        email: {
-          pattern: /[0-9a-z_-]+@[0-9a-z_-]+\.[a-z]{2,5}/i,
-        },
-      },
-      messages: {
-        name: {
-          required: "Please specify your name",
-          minlength: 2,
-        },
-        email: {
-          required: "We need your email address to contact you",
-          email: "Your email address must be in the format of name@domain.com",
-          pattern: 'Format for email: name@domain.com',
-        },
-        phone: {
-          required: "Please specify your phone",
-        }
-      },
-    });
-  });
-
-  // Ввод в input для имени только буквы
-  $('body').on('input', '.input-name', function(){
-    this.value = this.value.replace(/[^a-zа-яё\s]/gi, '');
-  });
-
-  // Маска для формы телефона
-  $('.phone_ru').mask('+7 (999) 999-99-99');
-
-
-  // Инициализация AOS анимации
-  AOS.init();
+$('body').on('input','.input-name',function(){
+  this.value=this.value.replace(/[^a-zа-яё\s]/gi,'')
+});
+$('.phone_ru').mask('+7 (999) 999-99-99');
+AOS.init();
 });
